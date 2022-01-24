Vanilla-Conquer/Vanilla-Conquer

[SS] Alters startup code to set resolution to 640x480.
Standard C&C resolution of 640x400 crashes due to larger VQA res.

solesurvivor/startup.cpp
@@ -150,7 +150,7 @@ int main(int argc, char** argv)
     /*
     **	Remember the current working directory and drive.
     */
-    Paths.Init("vanillatd", "CONQUER.INI", "CONQUER.MIX", args.ArgV[0]);
+    Paths.Init("vanillass", "CONQUER.INI", "CONQUER.MIX", args.ArgV[0]);
     vc_chdir(Paths.Program_Path());
     CDFileClass::Refresh_Search_Drives();
 
@@ -322,6 +322,7 @@ int main(int argc, char** argv)
             }
 #endif
         }
+        ScreenHeight = GBUFF_INIT_HEIGHT;
 
         SeenBuff.Attach(&VisiblePage, 0, 0, GBUFF_INIT_WIDTH, GBUFF_INIT_HEIGHT);
         HidPage.Attach(&HiddenPage, 0, 0, GBUFF_INIT_WIDTH, GBUFF_INIT_HEIGHT);

OmniBlade/Vanilla-Conquer-1

solesurvivor/startup.cpp
@@ -225,7 +225,7 @@ int main(int argc, char** argv)
     /*
     **	Remember the current working directory and drive.
     */
-    Paths.Init("vanillatd", "CONQUER.INI", "CONQUER.MIX", args.ArgV[0]);
+    Paths.Init("vanillass", "CONQUER.INI", "SOLE.MIX", args.ArgV[0]);
     vc_chdir(Paths.Data_Path());
     CDFileClass::Refresh_Search_Drives();
 
@@ -400,6 +400,7 @@ int main(int argc, char** argv)
             }
         }
 #endif
+        ScreenHeight = GBUFF_INIT_HEIGHT;
 
         SeenBuff.Attach(&VisiblePage, 0, 0, GBUFF_INIT_WIDTH, GBUFF_INIT_HEIGHT);
         HidPage.Attach(&HiddenPage, 0, 0, GBUFF_INIT_WIDTH, GBUFF_INIT_HEIGHT);